68540306fd8c5e147d815f91eb5f3155ff80610b
Eli Pandolfo <epandolf@ucsc.edu>
2019-09-03 21:24:20 -0700

    initial commit

---
+++ models.py
@@ -0,0 +1,107 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'injustice'
+    players_per_group = 2
+    num_rounds = 3
+
+    # Escojan aquí la dotac del comprador y los pagos recibidos por poseer el bien x
+    dotacion_inicial_A = 100
+    dotacion_inicial_B = 10
+    pagos_x_A = 40
+    pagos_x_B = 20
+    robo = 20
+
+    # dotacion_inicial = 100
+    # pagos_x = 20
+
+    # Instrucciones
+    instructions = 'injustice/Instructions.html'
+
+
+class Subsession(BaseSubsession):
+    def creating_session(self):
+        groups = self.get_groups()
+        for g in groups:
+            if g.precio_aceptado is True:
+                self.group_randomly(fixed_id_in_group=True)
+
+        for p in self.get_players():
+            p.dotacion_A -= self.session.config['treatment']*20
+            p.dotacion_B += self.session.config['treatment'] * 20
+
+
+class Group(BaseGroup):
+    # Precio
+    precio = models.FloatField(max=Constants.dotacion_inicial_A, min=0)
+
+    #######################################################################
+    extraccion = models.BooleanField(initial=False)
+    #######################################################################
+
+    # Variable de Aceptación (True) o Rechazo (False) del precio  --- caso tratado
+    precio_aceptado = models.BooleanField(widget=widgets.RadioSelect, choices=[
+        [True, 'Aceptar'], [False, 'Rechazar']],initial=False)
+
+    def compra_x(self):
+        players = self.get_players()
+        for p in players:
+            if p.bien_x is False:
+                p.bien_x = True
+            else:
+                p.bien_x = False
+
+    def set_payoffs(self):
+        # oTree no les permite usar los fields de player directamente si estan en la clase Group
+        # por ello, otree tiene algunas funciones que les pueden ayudar, por ej. get_player_by_role
+
+        jugador_A = self.get_player_by_role('A')
+        jugador_B = self.get_player_by_role('B')
+
+        if self.precio_aceptado is True:
+            #jugador_A.payoff = jugador_A.dotacion_A - self.precio + c(Constants.pagos_x_A) + (jugador_A.extraccion)*c(Constants.robo)
+            jugador_A.payoff = jugador_A.dotacion_A - self.precio + c(Constants.pagos_x_A) + (self.extraccion) * c(Constants.robo)
+            # El jugador A es el que extrae, asi que al hacer el calculo de pagos para B, necesitan usar el
+            # valor de lo extraido por A, no por B como estaba en su codigo anterior (ya que este seria 0 al no
+            # ser el que extrae)
+            #jugador_B.payoff = jugador_B.dotacion_B + self.precio - (jugador_A.extraccion)*c(Constants.robo)
+            jugador_B.payoff = jugador_B.dotacion_B + self.precio - (self.extraccion) * c(Constants.robo)
+
+        else:
+            #jugador_A.payoff = jugador_A.dotacion_A + (jugador_A.extraccion)*c(Constants.robo)
+            jugador_A.payoff = jugador_A.dotacion_A + (self.extraccion) * c(Constants.robo)
+            #jugador_B.payoff = jugador_B.dotacion_B + c(Constants.pagos_x_B) - (jugador_A.extraccion) * c(Constants.robo)
+            jugador_B.payoff = jugador_B.dotacion_B + c(Constants.pagos_x_B) - (self.extraccion)*c(Constants.robo)
+
+
+#######################################################
+class Player(BasePlayer):
+    # Variable para saber si alguien tiene o no el bien x (True lo tiene, False no)
+    bien_x = models.BooleanField()
+    dotacion_A = models.CurrencyField(initial=Constants.dotacion_inicial_A)
+    dotacion_B = models.CurrencyField(initial=Constants.dotacion_inicial_B)
+    neutral = models.CurrencyField(max=Constants.dotacion_inicial_A, min =0, initial=0)
+    #extraccion = models.BooleanField()
+    #extraccion = models.CurrencyField(max=Constants.dotacion_inicial_A, min =0, initial=0)
+    # Pago ronda anterior
+    pago_anterior = models.CurrencyField(initial=0)
+
+    def role(self):
+        if self.id_in_group == 1:
+            return 'A'
+        else:
+            return 'B'
+
+    # extraccion solo sería para los jugadores tipo A
+    def extraccion_max(self):
+        return self.dotacion_A
---
+++ pages.py
@@ -0,0 +1,172 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+# import config_values as cv
+
+# Primero reciben instrs, luego info de sus dots y despues transan (Ultimatum con chat). Al terminar la transac,
+# ven los results e inic otra ronda (en oontrol). En trat, hay una opc de seize al pasar las instrucs
+
+
+class Introduction(Page):
+    def before_next_page(self):
+        if self.player.role() == 'A':
+            self.player.bien_x = False
+        else:
+            self.player.bien_x = True
+
+        if self.round_number > 1:
+            self.player.bien_x = self.player.in_round(self.round_number - 1).bien_x
+            self.player.pago_anterior = self.player.in_round(self.round_number - 1).payoff
+            self.player.pago_anterior = self.player.in_round(self.round_number - 1).payoff
+###CAMBIO MÁS RECIENTE: EVALUARLO##########(16:10 martes 3 de setiembre)
+    def is_displayed(self):
+        if self.round_number == 1:
+           return True
+        elif self.round_number == 2:
+            return self.group.in_round(self.round_number - 1).precio_aceptado is False
+        else:
+            if self.group.in_round(self.round_number - 2).precio_aceptado is False:
+                return self.group.in_round(self.round_number - 1).precio_aceptado is False
+#################
+
+
+
+class Neutral(Page):
+    form_model = 'player'
+    form_fields = ['neutral']
+
+    def is_displayed(self):
+        if self.round_number == 1:
+            return True
+        else:
+            return False
+
+
+class Chat(Page):
+    timeout_seconds = 60
+
+    def is_displayed(self):
+
+      #  if self.round_number == 1:
+       #     return True
+        #else:
+         #   if self.group.in_round(self.round_number - 1).precio_aceptado is False:
+          #      return True
+           # else:
+            #    return False
+
+        if self.round_number == 1:
+           return True
+        elif self.round_number == 2:
+            return self.group.in_round(self.round_number - 1).precio_aceptado is False
+        else:
+            if self.group.in_round(self.round_number - 2).precio_aceptado is False:
+                return self.group.in_round(self.round_number - 1).precio_aceptado is False
+
+
+class Extraccion(Page):
+    #form_model = 'player'
+    form_model = 'group'
+    form_fields = ['extraccion']
+
+    # Solo se mostará si estamos en el tratamiento
+    def is_displayed(self):
+        if self.session.config['treatment'] == 0 and self.player.role() == 'A' and self.round_number == 1:
+            return False
+        elif self.session.config['treatment'] == 1 and self.player.role() == 'A' and self.round_number == 1:
+            return True
+
+class Robo_resultados(Page):
+    def is_displayed(self):
+        if self.session.config['treatment'] == 0 and self.player.role() == 'B' and self.round_number == 1:
+            return False
+        elif self.session.config['treatment'] == 1 and self.player.role() == 'B' and self.round_number == 1:
+            return True
+
+
+class Espera(WaitPage):
+    pass
+
+
+class Oferta(Page):
+    form_model = 'group'
+    form_fields = ['precio']
+
+    def is_displayed(self):
+        if self.round_number == 1:
+            return self.player.bien_x is False
+        elif self.round_number == 2:
+            if self.group.in_round(self.round_number - 1).precio_aceptado is False:
+                return self.player.bien_x is False
+            else:
+                return False
+        else:
+            if self.group.in_round(self.round_number - 2).precio_aceptado is False:
+                if self.group.in_round(self.round_number-1).precio_aceptado is False:
+                    return self.player.bien_x is False
+            else:
+                return False
+
+
+class Aceptar_Oferta(Page):
+    form_model = 'group'
+    form_fields = ['precio_aceptado']
+
+  #  def is_displayed(self):
+   #     return self.player.bien_x is True
+
+    def is_displayed(self):
+        if self.round_number == 1:
+            return self.player.bien_x is True
+        elif self.round_number == 2:
+            if self.group.in_round(self.round_number - 1).precio_aceptado is False:
+                return self.player.bien_x is True
+            else:
+                return False
+        else:
+            if self.group.in_round(self.round_number - 2).precio_aceptado is False:
+                if self.group.in_round(self.round_number-1).precio_aceptado is False:
+                    return self.player.bien_x is True
+            else:
+                return False
+
+
+
+
+
+class EsperaResultados(WaitPage):
+
+    def after_all_players_arrive(self):
+        self.group.set_payoffs()
+        if self.group.precio_aceptado is True:
+            self.group.compra_x()
+
+
+
+class Results(Page):
+    pass
+
+    def is_displayed(self):
+        if self.round_number == 1:
+           return True
+        elif self.round_number == 2:
+            return self.group.in_round(self.round_number - 1).precio_aceptado is False
+        else:
+            if self.group.in_round(self.round_number - 2).precio_aceptado is False:
+                return self.group.in_round(self.round_number - 1).precio_aceptado is False
+
+
+page_sequence = [
+    Introduction,
+    Neutral,
+    Extraccion,
+    Espera,
+    Robo_resultados,
+    Espera,
+    Chat,
+    Oferta,
+    Espera,
+    Aceptar_Oferta,
+    EsperaResultados,
+    Results
+]
